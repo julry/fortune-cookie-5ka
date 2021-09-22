beef16ed38f6d7caa805ac1be71ef4a5a95cddb7
Julia <iulia.ryseva@gmail.com>
2021-09-22 21:31:40 +0300

    change share

--- src/utils/getShareParams.js
+++ src/utils/getShareParams.js
@@ -1,16 +1,13 @@
-import { resolve } from "url";
-import {shareImg} from "../constants/images";
-
+import { isMobile } from "react-device-detect";
 export const getShareParams = (result) => {
     const url = [window.location.protocol, '//', window.location.host, window.location.pathname].join('');
 
     const shareDescription = `${result.title} - моя будущая профессия в компании «Пятёрочка»! Хочешь узнать, какая карьера ждёт тебя в топовой компании в сфере ритейла? Переходи по ссылке и получи предсказание.`
-    const shareImage = resolve(url, shareImg);
     const queryParams = new URLSearchParams();
+    const shareTitle = isMobile ? shareDescription : 'Кто ты в офисе «Пятёрочки»?';
     queryParams.append('url', url);
-    queryParams.append('title', shareDescription);
-    queryParams.append('image', shareImage);
-    queryParams.append("comment", shareDescription);
+    queryParams.append('title', shareTitle);
+    if (!isMobile) queryParams.append("comment", shareDescription);
     queryParams.append("noparse", 'true');
 
     return `http://vk.com/share.php?${queryParams.toString()}`;
