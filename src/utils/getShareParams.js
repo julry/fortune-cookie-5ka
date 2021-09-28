15822bfa709e047f8740b914876cb25bdbb3199c
Julia <iulia.ryseva@gmail.com>
2021-09-28 19:17:05 +0300

    upd build

--- src/utils/getShareParams.js
+++ src/utils/getShareParams.js
@@ -5,10 +5,12 @@ export const getShareParams = (result) => {
     const url = [window.location.protocol, '//', window.location.host, window.location.pathname].join('');
     const shareDescription = `${result.title} - моя будущая профессия в компании «Пятёрочка»! Хочешь узнать, какая карьера ждёт тебя в топовой компании в сфере ритейла? Переходи по ссылке и получи предсказание. А еще - регистрируйся на кейс-чемпионат «Пятёрочки» по предпринимательским идеям в ритейле #Стартапни - чтобы не только гадать, но и готовиться к карьерному взлету!`;
     const shareImage = resolve(url, shareImg);
+    const shareTitle = isMobile ? shareDescription : 'Кто ты в офисе "Пятерочки"?'
     const queryParams = new URLSearchParams();
     queryParams.append('url', url);
+    queryParams.append('title', shareTitle);
     !isMobile && queryParams.append("comment", shareDescription);
     queryParams.append('image', shareImage);
 
-    return `http://vk.com/share.php?${queryParams.toString()}`;
+    return `vk://vk.com/share.php?${queryParams.toString()}`;
 };
